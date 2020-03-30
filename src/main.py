094a4f3db89b25c7a607fbb6845b1bfed44d49f1
hernanjkd <hernanjkd@gmail.com>
2020-03-29 20:19:29 -0400

    endpoint payments

--- src/main.py
+++ src/main.py
@@ -1,33 +1,33 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-# from models import db
+from models import db, Users
 import requests
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# MIGRATE = Migrate(app, db)
-# db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+MIGRATE = Migrate(app, db)
+db.init_app(app)
 CORS(app)
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+
+@app.route('/payment/data', methods=['POST'])
+def payment_data():
+
+    j = request.get_json()
+
+    return jsonify(j)
+
 
 @app.route('/mailgun', methods=['POST'])
 def mailgun():
@@ -43,17 +43,17 @@ def mailgun():
         ok = requests.post(f'https://api.mailgun.net/v3/{domain}/messages',
             auth=('api', key),
             data={
-                'from': f'{domain} <mailgun@swapprofit.herokuapp.com>',
+                'from': 'PokerSocietyOnline<play@thepokersocietyonline.com>',
                 'to': email,
-                'subject': 'Testing',
+                'subject': 'Play Online',
                 'text': 'Hello World',
-                'html': '<h1>Hello World</h1>'
+                'html': '<h1>Mr Lou</h1><p>This is an automated generated email from your new app ;) reaching your hotmail and any other email address we send it to!</p><p>Send me later the email you want the players to receive with the title you want, and also what to write instead of "PokerSocietyOnline"</p>'
             }).status_code == 200
         logs.append({'email':email,'ok':ok})
 
     return jsonify(logs)
 
-# this only runs if `$ python src/main.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,16 +2,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    
+    referral_emails = db.relationship('Referrals', back_populates='user')
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return f'<Users {self.email}>'
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'referral_emails': [x.email for x in self.referral_emails]
+        }
+
+class Referrals(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    referral_id = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    
+    user = db.relationship('Users', back_populates='referral_emails')
+
+    def __repr__(self):
+        return f'<Referrals {self.referral_id}>'
